03e8e4b26aae4c423b097cbb8a150afae3c27734
fatinmohtar <106618780+fatinmohtar@users.noreply.github.com>
2022-06-03 22:50:41 +0800

    Update MainActivity3.java

--- app/src/main/java/com/fatin/zakat/MainActivity3.java
+++ app/src/main/java/com/fatin/zakat/MainActivity3.java
@@ -1,6 +1,7 @@
 package com.fatin.zakat;
 
 import android.content.Intent;
+import android.support.annotation.Keep;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -12,10 +13,27 @@ public class MainActivity3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
+        float tv = 0, tgv = 0, r=0 , cv = 0, w= 0;
+        double  zp = 0,tz = 0;
+
         Intent intent = getIntent();
-        float tv = intent.getFloatExtra("TOTAL VALUE",0);
-        double zp = intent.getDoubleExtra("Z PAYABLE",0);
-        double tz = intent.getDoubleExtra(MainActivity2.EXTRA_NUMBER,0);
+        w = intent.getFloatExtra("weight", 0);
+        r = intent.getFloatExtra("checked",0);
+        cv = intent.getFloatExtra("currentValue", 0);
+
+        if (r == 1){
+            tgv = w - 85;
+        }else if(r == 2){
+            tgv = w - 200;
+        }
+
+        //total value
+        tv = w * cv;
+        //zakat payable
+        zp = tgv * cv;
+        //total zakat
+        tz = zp * 0.025;
+
 
         TextView totalValue = (TextView) findViewById(R.id.totalValue);
         TextView zakatPayable = (TextView) findViewById(R.id.zakatPayable);
